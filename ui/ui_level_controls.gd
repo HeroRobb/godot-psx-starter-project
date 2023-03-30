f2dac314c426f30eb3b57de4a38cd1059edd8a65
HeroRobb <faustianmood@gmail.com>
2023-03-30 15:46:20 -0400

    Groundwork for example level functionality for 2.0

--- ui/ui_level_controls.gd
+++ ui/ui_level_controls.gd
@@ -16,20 +16,43 @@ enum UI {
 
 const HIDE_CONTROLS_KEY = "hide level ui"
 
+@export_category("Base Information")
 @export var starting_ui: UI = UI.CONTROLS
 @export var start_active: bool = true
+@export var show_base_functionality_labels: bool = true
+@export_category("Functionalities")
+@export var can_cycle_cameras: bool = true
+@export var can_auto_cycle_cameras: bool = true
+@export var can_show_ui_outside_viewport: bool = false
 
 var active: bool = false
 var hide_controls: bool = false : set = set_hide_controls
 
 var _current_ui: UI = UI.CONTROLS
 
+
+@onready var _change_scene_label: Label = %ChangeSceneLabel
+@onready var _favorite_level_label: Label = %FavoriteLevelLabel
+@onready var _save_load_label: Label = %SaveLoadLabel
+@onready var _exit_game_label: Label = %ExitGameLabel
+@onready var _hide_ui_label: Label = %HideUILabel
+@onready var _cycle_cameras_label: Label = %CycleCamerasLabel
+@onready var _auto_cycle_cameras_label: Label = %AutoCycleCamerasLabel
+@onready var _show_outside_ui_label: Label = %ShowOutsideUILabel
 @onready var _ui_label: Label = %UILabel
 @onready var _controls_container: VBoxContainer = %ControlsLabelContainer
 @onready var _ui_save: CenterContainer = %UISave
+@onready var _base_functionality_labels: Array = [
+	_change_scene_label,
+	_favorite_level_label,
+	_save_load_label,
+	_exit_game_label,
+	_hide_ui_label,
+]
 
 
 func _ready() -> void:
+	_show_functionality_labels()
 	active = start_active
 	switch_ui(starting_ui)
 	var saved_hide_ui = ResourceManager.get_global_data(HIDE_CONTROLS_KEY)
@@ -118,3 +141,15 @@ func _handle_load_input(event: InputEvent) -> void:
 		ResourceManager.load_game(3)
 	if event.is_action_pressed("ui_cancel"):
 		switch_ui(UI.CONTROLS)
+
+
+func _show_functionality_labels() -> void:
+	if not show_base_functionality_labels:
+		for label in _base_functionality_labels:
+			label.hide()
+	if can_cycle_cameras:
+		_cycle_cameras_label.show()
+	if can_auto_cycle_cameras:
+		_auto_cycle_cameras_label.show()
+	if can_show_ui_outside_viewport:
+		_show_outside_ui_label.show()
